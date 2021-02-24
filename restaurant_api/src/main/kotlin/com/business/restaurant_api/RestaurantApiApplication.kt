831af846178debfcbcc5eb025cf2be1330ddbd18
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2021-02-24 22:55:53 +0400

    update

--- restaurant_api/src/main/kotlin/com/business/restaurant_api/RestaurantApiApplication.kt
+++ restaurant_api/src/main/kotlin/com/business/restaurant_api/RestaurantApiApplication.kt
@@ -1,5 +1,6 @@
 package com.business.restaurant_api
 
+import com.business.restaurant_api.repositories.mealRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -41,82 +42,7 @@ fun main(args: Array<String>) {
 	runApplication<RestaurantApiApplication>(*args)
 }
 
-@Component
-internal class DataLoader(private val mealRepository: MealRepository) {
-	@PostConstruct
-	private fun loadData() {
-		val namesList: List<Meal> = Arrays.asList(
-			Meal("Balıq"),
-			Meal("Dolma"),
-			Meal("basdirma"),
-			Meal("kabab")
-		)
-		mealRepository.saveAll(namesList)
-	}
-}
-
-@RestController
-@RequestMapping("/meals")
-internal class RestApiDemoController(private val mealRepository: MealRepository) {
-	@get:GetMapping
-	val meals: Iterable<Meal?>
-		get() = mealRepository.findAll()
-
-	@GetMapping("/{id}")
-	fun getMealById(@PathVariable id: String): Optional<Meal?> {
-		return mealRepository.findById(id)
-	}
-
-	@PostMapping
-	fun postMeal(@RequestBody coffee: Meal): Meal {
-		return mealRepository.save(coffee)
-	}
-
-	@PutMapping("/{id}")
-	fun putMeal(
-		@PathVariable id: String,
-		@RequestBody coffee: Meal
-	): ResponseEntity<Meal> {
-		return if (mealRepository.existsById(id)) ResponseEntity(
-			mealRepository.save(coffee),
-			HttpStatus.OK
-		) else ResponseEntity(
-			mealRepository.save(coffee), HttpStatus.CREATED
-		)
-	}
-
-	@DeleteMapping("/{id}")
-	fun deleteMeal(@PathVariable id: String) {
-		mealRepository.deleteById(id)
-	}
-
-	@PatchMapping("/{id}")
-	fun patchMeal(
-		@PathVariable id: String,
-		@RequestBody coffee: Meal
-	): ResponseEntity<Meal> {
-		return if (mealRepository.existsById(id)) ResponseEntity(
-			mealRepository.save(coffee),
-			HttpStatus.OK
-		) else ResponseEntity(
-			mealRepository.save(coffee), HttpStatus.CREATED
-		)
-	}
-}
-
-internal interface MealRepository : CrudRepository<Meal?, String?>
-
-@Entity
-internal class Meal {
-	@Id
-	var id: String? = null
-	var name: String? = null
 
 
-	constructor(id: String?, name: String?) {
-		this.id = id
-		this.name = name
-	}
 
-	constructor(name: String?) : this(UUID.randomUUID().toString(), name) {}
-}
+
